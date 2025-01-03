67689a6ef661b2b442411e61d78d037a06691587
Ranjeev Grewal <ranjeev.grewal@arrow.ca>
2025-01-03 14:54:59 -0800

    Add project files.

---
+++ GUIDGeneratorConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GUIDGeneratorConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine($"Please enter a number here (0 for guids, 1 for uuids, 2 for ulids): ");
+
+int choice = Convert.ToInt32(Console.ReadLine());
+
+UIDHelper MyChoice = new((UIDHelperType)choice);
+
---
+++ GUIDGeneratorConsoleApp/UIDHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIDGeneratorConsoleApp
+{
+    public enum UIDHelperType
+    {
+        GUID = 0,
+        UUID = 1,
+        ULID = 2
+    }
+
+    public sealed class UIDHelper (UIDHelperType uidType)
+    {        
+        private UIDHelperType UIDType { get; init; } = uidType;
+    }
+}
